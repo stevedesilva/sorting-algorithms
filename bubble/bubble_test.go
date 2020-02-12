cfe56a4084515a3e0ccf78c4d78dc56f246562a8
Steve de Silva <7001268+stevedesilva@users.noreply.github.com>
2020-02-12 17:23:38 +0000

    add tests

--- bubble/bubble_test.go
+++ bubble/bubble_test.go
@@ -14,14 +14,14 @@ func TestBubbleSort_should_sort_sequence(t *testing.T) {
 	}
 
 	tests := []test{
-		// {
-		// 	input: []int{9, 8, 7, 6, 5, 4, 3, 2, 1, 0},
-		// 	want:  []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9},
-		// },
-		// {
-		// 	input: []int{4, 1, 3, 2, 5, 0},
-		// 	want:  []int{0, 1, 2, 3, 4, 5},
-		// },
+		{
+			input: []int{9, 8, 7, 6, 5, 4, 3, 2, 1, 0},
+			want:  []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9},
+		},
+		{
+			input: []int{4, 1, 3, 2, 5, 0},
+			want:  []int{0, 1, 2, 3, 4, 5},
+		},
 		{
 			input: []int{1, 9, 2, 5},
 			want:  []int{1, 2, 5, 9},
